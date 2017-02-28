4b7b0f5e8a81f5d704166e168aeab235ae2afd08
Zahradnik Jakub <jz051501@student.ath.edu.pl>
2017-02-28 18:29:41 +0100

    aS

--- Wskaźniki/Zaawansowane obiekty/Klasy.h
+++ Wskaźniki/Zaawansowane obiekty/Klasy.h
@@ -8,6 +8,14 @@
 
 using namespace std;
 
+class CGeometryManager;
+class CCircle;
+
+class CGeometryManager {
+public:
+	bool PrzecinajaSie(CCircle&, CCircle&);
+};
+
 class CFoo
 {
 	friend void Wypisz(CFoo*); //nie ma znaczenia jej lokalizacja w klasie
@@ -16,6 +24,18 @@ private:
 
 public:
 	CFoo() { _PrywatnyNapis = "Kocham C++!!!\n"; }
+	string ZmienNapis(string NowyNapis)
+	{
+		_PrywatnyNapis = NowyNapis;
+	}
+	void ZmienNapis2(string NowyNapis)
+	{
+		this->_PrywatnyNapis = NowyNapis;
+	}
+	void Wypisz()
+	{
+		cout << this->_PrywatnyNapis;
+	}
 };
 
 class CCircle
@@ -35,5 +55,8 @@ public:
 		_pktSrodka._y = fY;
 	}
 	
-	friend bool PrzecinajaSie(CCircle&, CCircle&);
+	friend bool CGeometryManager::PrzecinajaSie(CCircle&, CCircle&);
+	friend bool PrzecinajaSie(CCircle &, CCircle &);
+
 };
+
--- Wskaźniki/Zaawansowane obiekty/Zaawansowane obiekty.cpp
+++ Wskaźniki/Zaawansowane obiekty/Zaawansowane obiekty.cpp
@@ -6,6 +6,12 @@ int main()
 	CFoo Foo;
 	Wypisz(&Foo); //w klasie jest wsk na CFoo, wiêc przekazuje REF na obiekt Foo
 
+	CFoo *Obiekt = new CFoo;
+	
+	Obiekt->ZmienNapis2("Herold");
+	Obiekt->Wypisz();
+	delete Obiekt;
+
 	_getch();
     return 0;
 }
