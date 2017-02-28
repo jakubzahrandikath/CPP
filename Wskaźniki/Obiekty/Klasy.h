41d75a27955baeba8573a079c26e13282d2102f4
Zahradnik Jakub <jz051501@student.ath.edu.pl>
2017-02-28 16:40:43 +0100

    Tablica dynamiczna + Klasa z tab dyn

--- Wskaźniki/Obiekty/Klasy.h
+++ Wskaźniki/Obiekty/Klasy.h
@@ -1,1 +1,56 @@
 #pragma once
+#include<memory>
+
+//KLASA TWORZACA TABLICE DYN 1D
+class CIntArray 
+{
+	static const unsigned int DOMYSLNY_ROZMIAR = 5;
+
+private:
+	int *_wskTablica;
+	unsigned int _uRozmiar;
+public:
+	CIntArray()
+	{
+		_uRozmiar = DOMYSLNY_ROZMIAR;
+		_wskTablica = new int[_uRozmiar];
+	}
+	CIntArray(unsigned int rozmiar)
+	{
+		_uRozmiar = rozmiar;
+		_wskTablica = new int[_uRozmiar];
+	}
+	~CIntArray()
+	{
+		delete[] _wskTablica;
+	}
+
+	int Pobierz(int Indeks)
+	{
+		if (Indeks < _uRozmiar) return _wskTablica[Indeks];
+		else
+		{
+			return 0;
+		}
+	}
+	bool Ustaw(int Indeks, int Wartosc)
+	{
+		if (Indeks >= _uRozmiar)return false;
+		else
+			_wskTablica[Indeks] = Wartosc;
+	}
+	unsigned Rozmiar() { return _uRozmiar; }
+
+	bool ZmienRozmiar(unsigned NowyRozmiar)
+	{
+		if (!(NowyRozmiar) > _uRozmiar)return false;
+		
+		int *pNowaTablica = new int[NowyRozmiar];
+		memcpy(pNowaTablica, _wskTablica, _uRozmiar * sizeof(int));
+		delete[] _wskTablica;
+
+		_wskTablica = pNowaTablica;
+		_uRozmiar = NowyRozmiar;
+		return true;
+	}
+};
--- Wskaźniki/Obiekty/Obiekty.cpp
+++ Wskaźniki/Obiekty/Obiekty.cpp
@@ -1,8 +1,32 @@
 #include "stdafx.h"
 #include <iostream>
+#include "Klasy.h"
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
+	//TABLICA DYNAMICZNA
+	float *pfTablica;
+	pfTablica = new float[1024];
+	if (pfTablica == NULL)cout << "Brak pamiêci!\n";
+	for (int i = 0; i < 1024; i++)
+	{
+		pfTablica[i] = i * 2.0;
+	}
+
+	delete[] pfTablica;
+
+	//KLASA Z DYNAMIKA
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	CIntArray aTablica(rand());
+
+	for (int i = 0; i < aTablica.Rozmiar(); i++)
+	{
+		aTablica.Ustaw(i, rand());
+	}
     return 0;
 }
 
